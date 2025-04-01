f2d9d30015a53cd88c13fa38d2b9942e4a904922
SimonSchneider <dev@simonschneider.org>
2025-04-01 06:42:23 +0200

    fix: cant change name of once chore
    fixes #11

--- internal/core/chore_util_test.go
+++ internal/core/chore_util_test.go
@@ -11,6 +11,7 @@ import (
 	"github.com/SimonSchneider/goslu/srvu"
 	_ "github.com/ncruces/go-sqlite3/driver"
 	_ "github.com/ncruces/go-sqlite3/embed"
+	"golang.org/x/exp/slices"
 	"html/template"
 	"io"
 	"log"
@@ -77,6 +78,7 @@ func Setup() (context.Context, *Client, context.CancelFunc) {
 		LoginFailedRedirect:         "/login",
 		RedirectParam:               "redirect",
 		SessionsPath:                "/sessions/",
+		CSRFTokenFieldName:          "CSRFToken",
 		SessionCookie: auth.CookieConfig{
 			Name:        "session",
 			Expire:      1 * time.Hour,
@@ -105,6 +107,7 @@ func (c *Client) DBQuery() *cdb.Queries {
 type ClientToken struct {
 	cookieName string
 	val        string
+	CSRF       string
 }
 
 func (t ClientToken) Auth(r *http.Request) *http.Request {
@@ -118,10 +121,16 @@ func (c *Client) NewToken(ctx context.Context) (*ClientToken, error) {
 		return nil, err
 	}
 	token := core.NewId()
-	if err := c.tokenStore.StoreSession(ctx, u.ID, token, time.Now().Add(1*time.Hour)); err != nil {
+	csrfToken := core.NewId()
+	if err := c.tokenStore.StoreSession(ctx, auth.Session{
+		UserID:    u.ID,
+		Token:     token,
+		CSRFToken: csrfToken,
+		ExpiresAt: time.Now().Add(1 * time.Hour),
+	}); err != nil {
 		return nil, err
 	}
-	return &ClientToken{cookieName: c.authCookieName, val: token}, nil
+	return &ClientToken{cookieName: c.authCookieName, val: token, CSRF: csrfToken}, nil
 }
 
 func (c *Client) Serve(r *http.Request) *httptest.ResponseRecorder {
@@ -156,7 +165,11 @@ func (r *ChoreReq) Method(method string, uri string, body io.Reader) *ChoreReq {
 	return r
 }
 
-func (r *ChoreReq) Form(method string, uri string, form map[string]string) *ChoreReq {
+func (r *ChoreReq) Form(method, uri, csrf string, form map[string]string) *ChoreReq {
+	if form == nil {
+		form = make(map[string]string)
+	}
+	form["CSRFToken"] = csrf
 	r.req = NewFormReq(r.ctx, method, uri, form)
 	return r
 }
@@ -199,7 +212,7 @@ func (r *ChoreReq) DoAndFollow(status int) (*http.Response, error) {
 }
 
 func NewChoreList(ctx context.Context, client *Client, token *ClientToken, formVals map[string]string) (*core.ChoreListView, error) {
-	_, err := NewChoreReq(ctx, client).Auth(token).Form("POST", "/chore-lists/", formVals).DoAndFollow(http.StatusSeeOther)
+	_, err := NewChoreReq(ctx, client).Auth(token).Form("POST", "/chore-lists/", token.CSRF, formVals).DoAndFollow(http.StatusSeeOther)
 	if err != nil {
 		return nil, err
 	}
@@ -210,13 +223,38 @@ func NewChoreList(ctx context.Context, client *Client, token *ClientToken, formV
 	return &cl, nil
 }
 
+func findInSlice[T any](slice []T, pred func(T) bool) *T {
+	idx := slices.IndexFunc(slice, pred)
+	if idx == -1 {
+		return nil
+	}
+	return &slice[idx]
+}
+
 func NewChore(ctx context.Context, client *Client, token *ClientToken, formVals map[string]string) (*core.Chore, error) {
-	if _, err := NewChoreReq(ctx, client).Auth(token).Form("POST", "/chores/", formVals).DoAndFollow(http.StatusSeeOther); err != nil {
+	if _, err := NewChoreReq(ctx, client).Auth(token).Form("POST", "/chores/", token.CSRF, formVals).DoAndFollow(http.StatusSeeOther); err != nil {
 		return nil, err
 	}
-	chore := GetTpl[*core.Chore](client.tmpl, "chore-element.gohtml")
-	if chore == nil {
+	cl := GetTpl[core.ChoreListView](client.tmpl, "chore_list.page.gohtml")
+	if cl.Chores == nil {
 		return nil, fmt.Errorf("chore not found")
 	}
-	return chore, nil
+	c := findInSlice(cl.Chores.Chores, func(c core.Chore) bool {
+		return c.Name == formVals["name"]
+	})
+	return c, nil
+}
+
+func GetChore(ctx context.Context, client *Client, token *ClientToken, choreListID, choreID string) (*core.Chore, error) {
+	if _, err := NewChoreReq(ctx, client).Auth(token).Get(fmt.Sprintf("/chore-lists/%s", choreListID)).DoAndFollow(http.StatusOK); err != nil {
+		return nil, err
+	}
+	cl := GetTpl[core.ChoreListView](client.tmpl, "chore_list.page.gohtml")
+	if cl.Chores == nil {
+		return nil, fmt.Errorf("chore not found")
+	}
+	c := findInSlice(cl.Chores.Chores, func(c core.Chore) bool {
+		return c.ID == choreID
+	})
+	return c, nil
 }
