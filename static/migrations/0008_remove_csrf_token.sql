ae0e55296d75e7f20b19e97e4e15c53f86054eaa
SimonSchneider <dev@simonschneider.org>
2025-12-14 20:12:16 +0100

    add new migration to fix bug

---
+++ static/migrations/0008_remove_csrf_token.sql
@@ -0,0 +1,15 @@
+-- migrate:up
+CREATE TABLE tokens_new
+(
+    user_id    TEXT    NOT NULL,
+    token      TEXT    NOT NULL PRIMARY KEY,
+    expires_at INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE
+);
+
+INSERT INTO tokens_new (user_id, token, expires_at)
+SELECT user_id, token, expires_at FROM tokens;
+
+DROP TABLE tokens;
+
+ALTER TABLE tokens_new RENAME TO tokens;
